d3a31b3ff9cc83004792cee8585323c7fea06a40
Raphhrma <raphaelramanitra@gmail.com>
2026-07-20 18:33:10 -0400

    main.py local and normal setup with fastAPI

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+#FastAPI application instance (centralizing routes)
+app = FastAPI(title = "FastAPI Application", description = "This is a FastAPI application for IoT devices")
+
+#test route to check application status
+@app.get("/")
+def read_root():
+    return {"message": "Sentiom IOT Application is online"}
+
+#launch uvicorn server when running the app locally
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
